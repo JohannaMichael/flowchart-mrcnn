34c6b842182f560c82bbc5b798aed85c59c377a9
JohannaMichael <jm@cliomail.com>
2020-07-15 11:47:24 +0200

    Model ready to train.

--- TestSort.py
+++ TestSort.py
@@ -1,5 +1,4 @@
 from __future__ import print_function
-
 import glob
 import keras
 from keras.models import Sequential
@@ -11,6 +10,97 @@ from keras import backend as K, Model
 import numpy as np
 from keras.preprocessing import image
 import matplotlib.pyplot as plt
+import json
+import os
+import skimage.io
+import tensorflow as tf
+
+
+def load_doc(filename_data_input):
+    # open the file as read only
+    file = open(filename_data_input, 'r')
+    # read all text
+    text = file.read()
+    # close the file
+    file.close()
+    return text
+
+
+def load_descriptions(data):
+    sorted_flowchart_array = []
+    # process lines
+    for line in data.split('\n'):
+        # split lines by white space
+        tokens = line.split()
+        int_tokens = []
+        for symbols in tokens:
+            int_tokens.append(int(symbols))
+        sorted_flowchart_array.append(int_tokens)
+    return sorted_flowchart_array
+
+
+filename = './FlowchartDataMRCNN/TrainingImages/flowchart_symbol_order.txt'
+doc = load_doc(filename)
+sorted_array = load_descriptions(doc)
+
+flowcharts_symbols_array = []
+
+ROOT_DIR = os.path.abspath('./')
+dataset_dir = os.path.join(ROOT_DIR, 'FlowchartDataMRCNN/TrainingImages')
+
+annotations = json.load(open(os.path.join(dataset_dir, 'via_region_data.json')))
+annotations = list(annotations.values())  # don't need the dict keys
+
+# The VIA tool saves images in the JSON even if they don't have any
+# annotations. Skip unannotated images.
+annotations = [a for a in annotations if a['regions']]
+# Add images
+for a in annotations:
+    single_flowchart_symbol_array = []
+    flowchart_symbols = [r['region_attributes'] for r in a['regions']]
+
+    image_path = os.path.join(dataset_dir, a['filename'])
+
+    for i, p in enumerate(flowchart_symbols):
+
+        if p['flowchart_symbols'] == 'terminal_start':
+            single_flowchart_symbol_array.insert(i, 1)
+        elif p['flowchart_symbols'] == 'flowline':
+            single_flowchart_symbol_array.insert(i, 2)
+        elif p['flowchart_symbols'] == 'input':
+            single_flowchart_symbol_array.insert(i, 3)
+        elif p['flowchart_symbols'] == 'decision':
+            single_flowchart_symbol_array.insert(i, 4)
+        elif p['flowchart_symbols'] == 'process':
+            single_flowchart_symbol_array.insert(i, 5)
+        elif p['flowchart_symbols'] == 'terminal_end':
+            single_flowchart_symbol_array.insert(i, 6)
+        elif p['flowchart_symbols'] == 'process_end':
+            single_flowchart_symbol_array.insert(i, 7)
+        elif p['flowchart_symbols'] == 'process_start':
+            single_flowchart_symbol_array.insert(i, 8)
+        elif p['flowchart_symbols'] == 'connector':
+            single_flowchart_symbol_array.insert(i, 9)
+        elif p['flowchart_symbols'] == 'document':
+            single_flowchart_symbol_array.insert(i, 10)
+        elif p['flowchart_symbols'] == 'terminal':
+            single_flowchart_symbol_array.insert(i, 11)
+    flowcharts_symbols_array.append(single_flowchart_symbol_array)
+
+x_train_symbols = tf.keras.preprocessing.sequence.pad_sequences(
+    flowcharts_symbols_array, padding="post"
+)
+
+y_train_symbols_sorted = tf.keras.preprocessing.sequence.pad_sequences(
+    sorted_array, padding="post"
+)
+
+# print('Flowcharts Array: ')
+# print(padded_flowchart_symbols)
+# print(type(padded_flowchart_symbols))
+# print('Sorted Flowcharts Array: ')
+# print(padded_sorted_flowchart_symbols)
+# print(type(padded_sorted_flowchart_symbols))
 
 pathToTrainImages = './FlowchartDataMRCNN/TrainingImages/'
 # Create a list of all image names in the directory
@@ -19,90 +109,70 @@ img_names = glob.glob(pathToTrainImages + '*.jpg')
 train_images = []
 x_train_pics = []
 
-for i in img_names:  # img is list of full path names of all images
-    train_images.append(i)  # Add it to the list of train images
+for i in img_names:
+    train_images.append(i)
 
 for image_path in train_images:
     # Convert all the images to size 299x299 as expected by the inception v3 model
-    img = image.load_img(image_path, grayscale=True, target_size=(500, 500))
+    img = image.load_img(image_path, color_mode='grayscale', target_size=(250, 250))
     # print(np.shape(img))
     # Convert PIL image to numpy array
     x = image.img_to_array(img)
     x_train_pics.append(x)
+
+print(np.shape(x_train_pics))
+max_len = max([len(i) for i in x_train_symbols])
+print(max_len)
+print(np.shape(x_train_pics)[1])
 
 
 def create_cnn(width, height, depth):
     # initialize the input shape and channel dimension, assuming
     # TensorFlow/channels-last ordering
     input_shape = (height, width, depth)
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3),
-                     activation='relu',
-                     input_shape=input_shape))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    return model
+    cnn_model = Sequential()
+    cnn_model.add(Conv2D(32, kernel_size=(3, 3),
+                         activation='relu',
+                         input_shape=input_shape))
+    cnn_model.add(Conv2D(64, (3, 3), activation='relu'))
+    cnn_model.add(MaxPooling2D(pool_size=(2, 2)))
+    cnn_model.add(Dropout(0.25))
+    cnn_model.add(Flatten())
+    cnn_model.add(Dense(128))
+    cnn_model.add(Activation('relu'))
+    cnn_model.add(Dropout(0.5))
+    cnn_model.add(Dense(128))
+    cnn_model.add(Activation('relu'))
+    return cnn_model
 
 
 def create_mlp(dim):
-    model = Sequential()
-    model.add(Dense(128, input_shape=(dim,), activation="relu"))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.3))
-    model.add(Dense(128))
-    model.add(Activation('relu'))
-    return model
+    mlp_model = Sequential()
+    mlp_model.add(Dense(128, input_shape=(dim,), activation="relu"))
+    mlp_model.add(Activation('relu'))
+    mlp_model.add(Dropout(0.3))
+    mlp_model.add(Dense(128))
+    mlp_model.add(Activation('relu'))
+    return mlp_model
 
 
-mlp = create_mlp(43)
-cnn = create_cnn(500, 500, 1)
+mlp = create_mlp(max_len)
+cnn = create_cnn(np.shape(x_train_pics)[1], np.shape(x_train_pics)[2], np.shape(x_train_pics)[3])
 combinedInput = concatenate([mlp.output, cnn.output])
 x = Dense(128, activation="relu")(combinedInput)
-x = Dense(43)(x)
+x = Dense(max_len)(x)
 model = Model(inputs=[mlp.input, cnn.input], outputs=x)
 
 model.compile(loss=keras.losses.mean_squared_error,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 # train the model
-print("[INFO] training model...")
 model.fit(
-    x=[x_train, x_train_pics], y=y_train,
-    epochs=10, batch_size=8)
-# make predictions on the testing data
-print("[INFO] predicting house prices...")
+    x=[np.array(x_train_symbols), np.array(x_train_pics)], y=np.array(y_train_symbols_sorted),
+    epochs=30, batch_size=4, verbose=1)
+
 # preds = model.predict([testAttrX, testImagesX])
 
-
-n = 100000
-x_train = np.zeros((n, 43))
-for i in range(n):
-    x_train[i, :] = np.random.choice(13, 43, replace=True)
-
-# x_train = x_train.reshape(n, 4, 1)
-y_train = np.sort(x_train, axis=1)
-
-n = 1000
-x_test = np.zeros((n, 43))
-for i in range(n):
-    x_test[i, :] = np.random.choice(13, 43, replace=True)
-
-# x_test = x_test.reshape(n, 4, 1)
-y_test = np.sort(x_test, axis=1)
-
-print(x_train[0])
-print(y_train[0])
-print(x_train.shape)
-print(y_train.shape)
-
-input_shape = (43,)
 #
 # model = Sequential()
 # model.add(Dense(128, input_shape=input_shape))
