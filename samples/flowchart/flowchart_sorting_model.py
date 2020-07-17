2b1b252baba102d5f157b128b229c3a83becbd90
JohannaMichael <jm@cliomail.com>
2020-07-17 17:49:54 +0200

    Tested Neural Net and connected it to the MRCNN. Plus saved and loaded model.

--- samples/flowchart/flowchart_sorting_model.py
+++ samples/flowchart/flowchart_sorting_model.py
@@ -3,9 +3,6 @@ import glob
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Activation, Conv2D, MaxPooling2D, concatenate
-from keras.layers import Conv1D, MaxPooling1D, Reshape
-from keras.callbacks import ModelCheckpoint
-from keras.models import model_from_json
 from keras import backend as K, Model
 import numpy as np
 from keras.preprocessing import image
@@ -39,27 +36,22 @@ def load_descriptions(data):
     return sorted_flowchart_array
 
 
-filename = './FlowchartDataMRCNN/TrainingImages/flowchart_symbol_order.txt'
+filename = '../../FlowchartDataMRCNN/TrainingImages/flowchart_symbol_order.txt'
 doc = load_doc(filename)
 sorted_array = load_descriptions(doc)
 
 flowcharts_symbols_array = []
 
-ROOT_DIR = os.path.abspath('./')
-dataset_dir = os.path.join(ROOT_DIR, 'FlowchartDataMRCNN/TrainingImages')
-
-annotations = json.load(open(os.path.join(dataset_dir, 'via_region_data.json')))
+annotations = json.load(open('../../FlowchartDataMRCNN/TrainingImages/via_region_data.json'))
 annotations = list(annotations.values())  # don't need the dict keys
 
-# The VIA tool saves images in the JSON even if they don't have any
-# annotations. Skip unannotated images.
 annotations = [a for a in annotations if a['regions']]
 # Add images
 for a in annotations:
     single_flowchart_symbol_array = []
     flowchart_symbols = [r['region_attributes'] for r in a['regions']]
 
-    image_path = os.path.join(dataset_dir, a['filename'])
+    image_path = os.path.join('../../FlowchartDataMRCNN/TrainingImages/via_region_data.json', a['filename'])
 
     for i, p in enumerate(flowchart_symbols):
 
@@ -95,15 +87,7 @@ y_train_symbols_sorted = tf.keras.preprocessing.sequence.pad_sequences(
     sorted_array, padding="post"
 )
 
-# print('Flowcharts Array: ')
-# print(padded_flowchart_symbols)
-# print(type(padded_flowchart_symbols))
-# print('Sorted Flowcharts Array: ')
-# print(padded_sorted_flowchart_symbols)
-# print(type(padded_sorted_flowchart_symbols))
-
-pathToTrainImages = './FlowchartDataMRCNN/TrainingImages/'
-# Create a list of all image names in the directory
+pathToTrainImages = '../../FlowchartDataMRCNN/TrainingImages/'
 img_names = glob.glob(pathToTrainImages + '*.jpg')
 
 train_images = []
@@ -171,39 +155,26 @@ model.fit(
     x=[np.array(x_train_symbols), np.array(x_train_pics)], y=np.array(y_train_symbols_sorted),
     epochs=30, batch_size=4, verbose=1)
 
-# preds = model.predict([testAttrX, testImagesX])
+model.save('../../model_sorting_weights/model_weights.h5')
 
-#
-# model = Sequential()
-# model.add(Dense(128, input_shape=input_shape))
-# model.add(Activation('relu'))
-# model.add(Dropout(0.3))
-# model.add(Dense(128))
-# model.add(Activation('relu'))
-# # model.add(Dropout(0.15))
-# # model.add(Dense(43))
-#
-# model.compile(loss=keras.losses.mean_squared_error,
-#               optimizer=keras.optimizers.Adadelta(),
-#               metrics=['accuracy'])
-#
-# epochs = 10
-# batch_size = 128
-# # Fit the model weights.
-# history = model.fit(x_train, y_train,
-#                     batch_size=batch_size,
-#                     epochs=epochs,
-#                     verbose=1,
-#                     validation_data=(x_test, y_test))
-#
-# test_list = [[10, 2, 5, 12, 4, 3, 9, 3, 6, 8, 11, 12, 12, 1, 0, 1, 8, 2,
-#               0, 6, 1, 9, 8, 9, 11, 0, 2, 6, 11, 5, 0, 1, 7, 10, 11, 8,
-#               0, 3, 11, 11, 8, 8, 7]]
-# print(np.shape(test_list))
-# print(np.asarray(test_list))
-# print(np.shape(test_list))
-# pred = model.predict(np.asarray(test_list))
-# print(test_list)
-# print(pred)
-# print([np.asarray(test_list).reshape(43, )[np.abs(np.asarray(test_list).reshape(43, ) - i).argmin()] for i in
-#        list(pred[0])])
+
+pathToTrainImage = '../../FlowchartDataMRCNN/TestingImages/00000077.jpg'
+x_test_pics = []
+img = image.load_img(pathToTrainImage, color_mode='grayscale', target_size=(250, 250))
+# print(np.shape(img))
+# Convert PIL image to numpy array
+x = image.img_to_array(img)
+x_test_pics.append(x)
+
+y_test_symbols_unsorted = [[6, 1, 5, 5, 5, 2, 4, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                            0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+y_test_symbols_sorted = [[1, 2, 5, 2, 5, 2, 4, 2, 5, 2, 6, 2, 0, 0, 0,
+                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+prediction = model.predict([np.asarray(y_test_symbols_unsorted), np.asarray(x_test_pics)])
+print(prediction)
+print([np.asarray(y_test_symbols_unsorted).reshape(max_len, )
+       [np.abs(np.asarray(y_test_symbols_unsorted).reshape(max_len, ) - i).argmin()] for i in list(prediction[0])])
